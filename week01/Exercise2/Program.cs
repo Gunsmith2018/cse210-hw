713d25c86b92ba9f7c8e9bba959f75bbbc0d4317
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-07-04 13:08:24 -0500

    Made a grading system for exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please tell me your grade percentage ");
+        string reply = Console.ReadLine();
+        int percent = int.Parse(reply);
+
+        string greade_level = "";
+
+        if (percent >= 90)
+        {
+            greade_level = "A";
+        }
+        else if (percent >= 80)
+        {
+            greade_level = "B";
+        }
+        else if (percent >= 70)
+        {
+            greade_level = "C";
+        }
+        else if (percent >= 60)
+        {
+            greade_level = "D";
+        }
+        else
+        {
+            greade_level = "f";
+        }
+
+        Console.WriteLine($" Your grade is {greade_level}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You have passed this course");
+        }
+
+        else
+        {
+            Console.WriteLine("You have failed this course. Please try again. ");
+        }
+
     }
 }
