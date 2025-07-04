4de0f63f0a2c043b652531e91502ced27361470c
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-07-04 13:49:16 -0500

    Made a simple guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+            {
+                Console.Write("Please guess a number:  ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Try hiher ");
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Try lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it! ");
+                }
+
+
+            }
+
+
     }
 }
