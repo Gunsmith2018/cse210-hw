e1c553408bbb3fe4b79b7f945c6e8c6ec3a39832
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-07-05 11:28:55 -0500

    This program asks for a name an a number. it will square the number (multiply it by itself)

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        welcomemessage();
+        string name = username();
+        int inputnumber = usernumber();
+        int squarednumber = square(inputnumber);
+        results(name, squarednumber);
+
+        void welcomemessage()
+        {
+            Console.WriteLine("Welcome to a simple program with functions.");
+        }
+
+        string username()
+        {
+            Console.Write("Please tell me your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        int usernumber()
+        {
+            Console.Write("Please give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        int square(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        void results(string name, int squarednumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square} ");
+
+        }
+
     }
 }
