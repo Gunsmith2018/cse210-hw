cf7cf7ce4ab9defd65a3671426a92815fbe34d34
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-07-05 10:32:55 -0500

    Made a list to find average and maximum

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+    List<int> numbers;
+    numbers = new List<int>();
+
+    int user = -1;
+        while (user != 0)
+        {
+            Console.Write("Please enter a number. When you are done please enter (0): ");
+
+            string Response = Console.ReadLine();
+            user = int.Parse(Response);
+            if (user != 0)
+            {
+                numbers.Add(user);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($" The current sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
