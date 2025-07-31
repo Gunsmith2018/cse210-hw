07d7acd935e59985494b249b344a76e24cd7388d
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-07-31 15:36:22 -0500

    Working on week 5 program

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,70 @@
+public class Activity
+{
+    protected string _name = "";
+    protected string _description = "";
+    protected string _duration = "";
+
+
+
+
+    public Activity()
+    {
+        _name = "";
+        _description = "";
+        _duration = "";
+    }
+    public Activity(string name, string description, string duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+
+    public Activity()
+    {
+
+    }
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(seconds int)
+    {
+
+    }
+
+    public void ShowCountDown(seconds int)
+    {
+
+    }
+
+
+
+
+
+
+
+
+    
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+}
